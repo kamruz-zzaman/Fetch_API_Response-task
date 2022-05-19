cd4529e64b53d22df966ac432760b86ca99182ac
kamruz-zzaman <kamruz.zaman8991@gmail.com>
2022-05-19 22:57:59 +0600

    fetch data with js done

---
+++ app.js
@@ -0,0 +1,13 @@
+
+// token and api
+const api = "https://gorest.co.in/public/v1/users";
+const token = "d7c01847de4c083cb154e9a533294301e9f05f93dbae7d589e42ece63226c0a3";
+
+// fetch data
+fetch(api, {
+    headers: {
+        Authorization: { token }
+    }
+})
+    .then(res => res.json())
+    .then(data => console.log(data))
